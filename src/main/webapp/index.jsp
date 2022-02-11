bac3350c8ae4d5046bd8ae9c5501ea77be0db47b
hasee <hasee@qq.com>
2022-02-11 18:06:30 +0800

    已经SSM配置完成，纯净版

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: HASEE
+  Date: 2022/2/11
+  Time: 11:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<script type="text/javascript" src="static/js/jquery-1.12.4.min.js"></script>
+<link href="static/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+
+<script src="static/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<body>
+<h1>首页</h1>
+<button class="btn btn-success">按钮</button>
+
+</body>
+</html>
---
+++ src/test/java/MapperTest.java
@@ -0,0 +1,51 @@
+import com.atguigu.crud.beans.Department;
+import com.atguigu.crud.beans.Employee;
+import com.atguigu.crud.dao.DepartmentMapper;
+import com.atguigu.crud.dao.EmployeeMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import java.util.UUID;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:Application.xml"})
+public class MapperTest {
+
+    @Autowired
+    DepartmentMapper departmentMapper;
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    @Autowired
+    SqlSession sqlSession;
+
+    @Test
+    public void test1(){
+
+       // System.out.println(departmentMapper);
+
+//        departmentMapper.insertSelective(new Department(null,"开发部"));
+//        departmentMapper.insertSelective(new Department(null,"测试部"));
+//        Department department = departmentMapper.selectByPrimaryKey(1);
+//        List<Department> departments = departmentMapper.selectByExample(null);
+//        System.out.println(departments);
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+//        for(int i = 0;i<1000;i++){
+//            String uid = UUID.randomUUID().toString().substring(0,5)+i;
+//            mapper.insertSelective(new Employee(null,uid,"M", uid+"@atguigu.com", 1, null));
+//        }
+        List<Employee> employees = mapper.selectByExample(null);
+        System.out.println(employees);
+        System.out.println("批量完成");
+
+
+
+    }
+
+}
